cd6b3e94aa2367c72e4278c5eec975bfb1812f95
fengloop186 <socbel25@gmail.com>
2026-05-13 08:52:03 +0700

    String

---
+++ 02_String/07/6_string.py
@@ -0,0 +1,19 @@
+strFeng = "I'am Jeng\nFree game \nShare tobe better"
+print(strFeng)
+print(type(strFeng))
+#string
+#STR
+#Alert: \a (Phat ra mot tieng bip)
+#Baackspace: \b (Dua con tro tro ve mot khoang trang)
+#Newline: \n (Dua con tro xuong dong tiep theo)
+#Horizontal tab: \t (In mot horizotal tab)
+#Single quote: \' (In ra ki tu ')
+#Double quote: \" (In ra ki tu ")
+#Blacklash: \\ (In ra ki tu \)
+print('\a')
+print('feng\b')
+print('loop\n')
+print('jeng\thow')
+print('\'')
+print('\"')
+print('\\')
